23cbc80f669e169f1b61887a70a9c8ff628368c2
LoudnessAM <adrienne.molnar@icloud.com>
2025-05-07 22:39:02 +0200

    Ich ficke deine Mutter

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -144,7 +144,7 @@ CORS_ALLOW_CREDENTIALS = True
 # Sicherheitseinstellungen für Produktion
 if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_HSTS_SECONDS = 31536000  # 1 Jahr
